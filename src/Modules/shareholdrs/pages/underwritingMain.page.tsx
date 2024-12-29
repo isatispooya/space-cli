9f5097787aff8a484fa0db62dafecf6ecd5fa17f
mhmdalisadat <150137653+mhmdalisadat@users.noreply.github.com>
2024-12-29 13:11:40 +0330

    45454

--- src/Modules/shareholdrs/pages/underwritingMain.page.tsx
+++ src/Modules/shareholdrs/pages/underwritingMain.page.tsx
@@ -36,7 +36,7 @@ const UnderwritingMain = () => {
     <MainLayout>
       <div className="min-h-screen flex flex-col">
         <div className="bg-white p-3">
-          <h1 className="text-2xl font-bold text-center text-indigo-600 mb-1">
+          <h1 className="text-2xl font-bold text-center text-[#5677BC] mb-1">
             پذیره نویسی
           </h1>
           <p className="text-gray-600 text-center text-sm">
