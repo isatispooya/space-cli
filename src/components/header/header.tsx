dd70ed2a64346447f202b7aac641be0e993b4be5
mhmdalisadat <150137653+mhmdalisadat@users.noreply.github.com>
2025-01-02 12:45:32 +0330

    555

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,11 +6,9 @@ import LogoText from "../../assets/textLogo.png";
 import { useSidebarStore } from "../../Modules/sidebar/store/sidebar.store";
 import { FiMenu } from "react-icons/fi";
 
-
 initTWE({ Collapse, Ripple });
 
 const Header = () => {
-  
   const { toggleSidebar } = useSidebarStore();
   return (
     <header>
@@ -19,44 +17,51 @@ const Header = () => {
         className="relative shadow-2xl flex w-full items-center justify-between bg-white py-2 dark:bg-body-dark lg:flex-wrap lg:justify-start lg:py-4"
         data-twe-navbar-ref
       >
-
         <div className="flex w-full flex-wrap items-center justify-between px-3">
           <div className="flex items-center">
             <div className="max-w-xs h-14 flex items-center">
-            <div className="flex h-full items-center justify-center m-4">
-              <p
-                onClick={toggleSidebar}
-                className="text-4xl cursor-pointer text-gray-500 hover:text-gray-700 transition-colors duration-200"
-              >
-                <FiMenu />
-              </p>
+              <div className="flex h-full items-center justify-center m-4">
+                <p
+                  onClick={toggleSidebar}
+                  className="text-4xl cursor-pointer text-gray-500 hover:text-gray-700 transition-colors duration-200"
+                >
+                  <FiMenu />
+                </p>
+              </div>
+              <div className=" max-w-xs h-14 flex items-center">
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  className="flex items-center"
+                >
+                  <img
+                    src={LogoWhite}
+                    className="w-20 hidden lg:block"
+                    alt="logo"
+                  />
+                  <img
+                    src={LogoText}
+                    className="w-40 hidden lg:block"
+                    alt="logo text"
+                  />
+                </motion.div>
+              </div>
             </div>
-            <div className=" max-w-xs h-14 flex items-center">
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                className="flex items-center"
-              >
-                <img src={LogoWhite} className="w-20 hidden lg:block" alt="logo" />
-                <img src={LogoText} className="w-40 hidden lg:block" alt="logo text" />
-              </motion.div>
+
+            <div
+              className="!visible hidden grow basis-[100%] items-center text-center lg:!flex lg:basis-auto lg:text-left"
+              id="navbarSupportedContentY"
+              data-twe-collapse-item
+            ></div>
+            <h1 className="text-md  text-[#5677BC] ml-7">
+              نرم افزار تحت وب ایساتیس من
+            </h1>
+
+            <div className="flex items-center ml-8 space-x-4">
+              <Avatar />
             </div>
           </div>
-
-          <div
-            className="!visible hidden grow basis-[100%] items-center text-center lg:!flex lg:basis-auto lg:text-left"
-            id="navbarSupportedContentY"
-            data-twe-collapse-item
-          ></div>
-          <h1 className="text-md  text-[#5677BC] ml-7">
-            نرم افزار تحت وب ایساتیس من
-          </h1>
-
-          <div className="flex items-center ml-8 space-x-4">
-            <Avatar />
-          </div>
-        </div>
         </div>
       </nav>
     </header>
