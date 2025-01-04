f724f61c11f0ec3d931388317c84f0ff13a79036
zahraAm <zahraamini.yazd20@gmai.com>
2025-01-04 12:31:07 +0330

    Export additional notification hooks for improved notification management

--- src/components/notification/index.ts
+++ src/components/notification/index.ts
@@ -1,1 +1,4 @@
 export { default as Notification } from "./notification";
+export { useMarkAsRead } from "./hook/notification.get";
+export { default as useNotifications } from "./hook/notification.get";
+export { default as useNotificationsList } from "./hook/notification.get";
