3b95a342b95881bf191681e3d1afac0285be1708
zahraAm <zahraamini.yazd20@gmai.com>
2025-01-08 14:28:45 +0330

    Enhance signup and registration processes by adding referral handling
    - Introduced 'referal' field in SignupForm to capture referral information during user signup.
    - Updated postOtp and postRegister services to include 'referal' in API requests for better tracking of user sources.
    - Added console log in postOtp for improved debugging of OTP responses.
    
    These changes aim to improve user onboarding and facilitate referral tracking in the application.

--- src/Modules/auth/services/register.post.ts
+++ src/Modules/auth/services/register.post.ts
@@ -4,13 +4,16 @@ import { server } from "../../../api/server";
 const postRegister = async ({
   nationalCode,
   otpValue,
+  referal,
 }: {
   nationalCode: string;
   otpValue: string;
+  referal: string;
 }) => {
   const response = await api.post(`${server}/register/`, {
     uniqueIdentifier: nationalCode,
     otp: otpValue,
+    referal,
   });
   return response.data;
 };
