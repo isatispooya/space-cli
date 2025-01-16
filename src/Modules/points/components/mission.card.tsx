f9f5c3818be1d5ca60f15f6f0ae905efcbc17327
zahraAm <zahraamini.yazd20@gmai.com>
2025-01-16 11:27:30 +0330

    Refactor MissionCard component for improved layout and styling
    - Adjusted the width of the MissionCard to 90% for better responsiveness.
    - Updated image dimensions to 130x130 pixels for consistent display.
    - Modified the text container width to 2/3 for better alignment.
    - Increased button width to 330px for enhanced usability.
    
    These changes enhance the visual consistency and user experience of the MissionCard component.

--- src/Modules/points/components/mission.card.tsx
+++ src/Modules/points/components/mission.card.tsx
@@ -8,18 +8,18 @@ const MissionCard = ({ missions }: { missions: MissionTypes[] }) => {
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-12">
       {missions.map((item, index) => (
         <motion.div
-          className="flex flex-col bg-with border rounded-2xl shadow-md hover:shadow-lg transition-shadow duration-300"
-          style={{ width: '100%' }}
+          className="flex bg-with border rounded-2xl shadow-md hover:shadow-lg transition-shadow duration-300"
+          style={{ width: '90%' }}
           key={index}
         >
-          <div className="mb-[-40px]">
+          <div className=" mb-[-40px]">
             <img
               src={item.image}
               alt="Mission"
-              className="w-full h-auto rounded-xl object-cover m-2"
+              className="w-[130px] h-[130px] rounded-xl object-cover m-2"
             />
           </div>
-          <div className="flex flex-col justify-between p-2 w-full text-right">
+          <div className="flex flex-col justify-between p-2  w-2/3 text-right">
             <h2 className="text-lg font-bold text-gray-800 mb-1">
               {item.display_name}
             </h2>
@@ -36,13 +36,16 @@ const MissionCard = ({ missions }: { missions: MissionTypes[] }) => {
             </div>
             <div className="flex justify-center">
               <a href={item.link} target="_blank">
-                <button className="mt-2 bg-gray-200 text-gray-800 py-2 px-2 rounded-lg text-sm hover:bg-gray-300 transition-colors duration-300 w-full">
+                <button className="mt-2 bg-gray-200 text-gray-800 py-2 px-2 rounded-lg text-sm hover:bg-gray-300 transition-colors duration-300 w-[330px]">
                   رفتن به ماموریت
                 </button>
               </a>
             </div>
           </div>
+          
         </motion.div>
+
+        
       ))}
     </div>
   );
