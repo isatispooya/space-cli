284b98d92bcb5677e89c3f4bd5a41657e92bb398
zahraAm <zahraamini.yazd20@gmai.com>
2025-01-19 12:07:58 +0330

    Enhance useGifts hook by integrating useRemainPoints for refetching on successful gift submissions. Update error handling to ensure consistent error message formatting.

--- src/Modules/points/hooks/useGifts.ts
+++ src/Modules/points/hooks/useGifts.ts
@@ -8,6 +8,7 @@ import { giftServices } from "../services";
 import { GiftsPost, GiftTypes } from "../types";
 import { AxiosError } from "axios";
 import toast from "react-hot-toast";
+import useRemainPoints from "./useRemainPoints";
 
 const useGifts = {
   useGetGifts: (): UseQueryResult<GiftTypes[]> => {
@@ -21,14 +22,17 @@ const useGifts = {
     AxiosError,
     { id: string; gift: string; description: string }
   > => {
+    const { refetch } = useRemainPoints();
+
     return useMutation({
       mutationKey: ["postGift"],
       mutationFn: (data: { id: string; gift: string; description: string }) =>
         giftServices.postGift(data),
       onSuccess: (response) => {
         toast.success(response.message || "دریافت هدیه با موفقیت انجام شد");
+        refetch();
       },
-      onError: (error: AxiosError<{error: string}>) => {
+      onError: (error: AxiosError<{ error: string }>) => {
         const errorMessage = error.response?.data?.error || "خطایی رخ داده است";
         toast.error(errorMessage);
       },
