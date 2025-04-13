ccb401f466b7721b135c6f455d06e4cb78731e86
mhmdalisadat <150137653+mhmdalisadat@users.noreply.github.com>
2025-04-13 15:19:20 +0330

    Add TimeflowEdit component and enhance UserTimeflowTable with action menu for row interactions

---
+++ src/Modules/timeflow/components/timeflow_edit.tsx
@@ -0,0 +1,16 @@
+import { Forms } from "@/components";
+
+const TimeflowEdit = () => {
+  return (
+    <div>
+      <Forms
+        formFields={[]}
+        initialValues={{}}
+        validationSchema={}
+        onSubmit={}
+      />
+    </div>
+  );
+};
+
+export default TimeflowEdit;
