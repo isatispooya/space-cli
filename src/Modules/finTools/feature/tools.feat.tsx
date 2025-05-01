adb068944bb4da1323c909dcce6bd14d22371eb9
mhmdalisadat <150137653+mhmdalisadat@users.noreply.github.com>
2025-05-01 12:21:51 +0330

    Update FundCard component to handle missing symbol data gracefully: Introduce fallback for image and name display, and clean up unused code in ToolsFeat component.

--- src/Modules/finTools/feature/tools.feat.tsx
+++ src/Modules/finTools/feature/tools.feat.tsx
@@ -1,20 +1,14 @@
 import { motion } from "framer-motion";
 import FundCard from "../components/cards";
 import { useSymbols } from "../hooks";
-import { Symbol } from "../types";
 
 export const ToolsFeat: React.FC = () => {
   const { data: symbols } = useSymbols.useGetSymbols();
+
   const itemVariants = {
     hidden: { opacity: 0, y: 20 },
     visible: { opacity: 1, y: 0 },
   };
-
-  const khatamSymbol = symbols
-    ? Object.values(symbols).find(
-        (symbol: Symbol) => symbol.symbol_detail?.symbol === "خاتم"
-      )
-    : null;
 
   return (
     <div className="min-h-screen w-full p-2 md:p-4 flex flex-col gap-4">
@@ -25,13 +19,13 @@ export const ToolsFeat: React.FC = () => {
         transition={{ duration: 0.3, delay: 0.3 }}
         className="w-full h-[35vh]"
       >
-        <FundCard
-          symbol={khatamSymbol}
-          value={950000}
-          change={12000}
-          changePercentage={1.5}
-          size="large"
-        />
+        {/* <FundCard
+            symbol={symbols}
+            value={950000}
+            change={12000}
+            changePercentage={1.5}
+            size="large"
+          /> */}
       </motion.div>
     </div>
   );
