d392a369005b9b47ad62d3e1de625495f02f8e55
نام کاربری شما < >
2025-05-03 13:01:51 +0330

    Refactor ToolsFeat component: Update import statements for better clarity and structure, and integrate useSymbols hook to fetch symbols data for improved functionality.

--- src/Modules/finTools/feature/tools.feat.tsx
+++ src/Modules/finTools/feature/tools.feat.tsx
@@ -1,9 +1,11 @@
 import { motion } from "framer-motion";
 import FundCard from "../components/cards";
-import { useSymbols } from "../hooks";
-import { Symbol } from "../types/symbols.type";
+import type { Symbol } from "../types/symbols.type";
+import useSymbols from "../hooks/useSymbols";
+
 
 export const ToolsFeat: React.FC = () => {
+  const { data: symbols } = useSymbols.useGetSymbols();
 
   const itemVariants = {
     hidden: { opacity: 0, y: 20 },
