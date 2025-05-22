31b82af43e29de8b72171904ab9d550d42a02efe
ZahraAmini1112 <zahraamini.yazd20@gmail.com>
2025-05-22 12:46:03 +0330

    announcements

--- src/Modules/auth/features/login.view.tsx
+++ src/Modules/auth/features/login.view.tsx
@@ -37,8 +37,11 @@ const Login: React.FC = () => {
       return () => clearInterval(timer);
     }
   }, [announcements]);
+  const filteredData = announcements.filter(
+    (item: { show_in: string }) => item.show_in === "my_isatis"
+  );
 
-  const currentAnnouncement = announcements?.[currentAnnouncementIndex];
+  const currentAnnouncement = filteredData?.[currentAnnouncementIndex];
 
   const handleComponentChange = (
     component: "login" | "signup" | "forgetpass"
